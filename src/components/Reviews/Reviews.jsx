a9e6524ed46b1f9bbacfa89ab1fb31b8f1a093da
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-08-02 01:39:20 +0300

    src-add

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,27 @@
+// import s from 'components/Navigation/MoviesPage.module.css';
+// import { useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Reviews = ({ moviesIdReview }) => {
+  return (
+    <>
+      {moviesIdReview.length === 0 ? (
+        <p>No reviews</p>
+      ) : (
+        moviesIdReview &&
+        moviesIdReview.map(review => (
+          <div key={review.id}>
+            <h3>{review.author}</h3>
+            <p>{review.content}</p>
+          </div>
+        ))
+      )}
+    </>
+  );
+};
+
+Reviews.propTypes = {
+  moviesIdReview: PropTypes.array.isRequired,
+};
+
+export default Reviews;
